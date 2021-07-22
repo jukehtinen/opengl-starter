3eaa788f421392f813d8cf53d5dc7b6ca18ce5df
jukehtinen <jukehtinen@gmail.com>
2021-07-22 15:01:32 +0300

    Minor code style changes.

--- opengl_starter/GltfLoader.cpp
+++ opengl_starter/GltfLoader.cpp
@@ -6,143 +6,144 @@
 #include "cgltf.h"
 #pragma warning(pop)
 
-using namespace opengl_starter;
-
-struct Vertex
+namespace opengl_starter
 {
-    float pos[3];
-    float uv[2];
-    float normal[3];
-};
-
-void GltfLoader::Load(const std::string& filename, opengl_starter::Node* parentNode, std::vector<opengl_starter::Mesh*>& outMeshes)
-{
-    cgltf_options options = {};
-    cgltf_data* data = nullptr;
-    cgltf_result result = cgltf_parse_file(&options, filename.c_str(), &data);
-    if (result != cgltf_result_success)
+    struct Vertex
     {
-        spdlog::error("[cgltf] Failed to load file {}", filename);
-        return;
-    }
+        float pos[3];
+        float uv[2];
+        float normal[3];
+    };
 
-    result = cgltf_load_buffers(&options, data, nullptr);
-
-    // Have another vector to avoid name collisions.
-    std::vector<opengl_starter::Mesh*> meshes;
-
-    for (int m = 0; m < data->meshes_count; m++)
+    void GltfLoader::Load(const std::string& filename, opengl_starter::Node* parentNode, std::vector<opengl_starter::Mesh*>& outMeshes)
     {
-        const auto& mesh = data->meshes[m];
-        spdlog::info("{}, {}", mesh.name, mesh.primitives_count);
-        for (int p = 0; p < mesh.primitives_count; p++)
+        cgltf_options options = {};
+        cgltf_data* data = nullptr;
+        cgltf_result result = cgltf_parse_file(&options, filename.c_str(), &data);
+        if (result != cgltf_result_success)
         {
-            if (p > 0)
-                spdlog::warn("[GltfLoader] {} has multiple primitives!", mesh.name);
-
-            const auto& primitive = mesh.primitives[p];
-
-            std::vector<uint32_t> indices(primitive.indices->count);
-            for (auto v = 0; v < primitive.indices->count; v += 3)
-            {
-                for (auto i = 0; i < 3; i++)
-                {
-                    indices[v + i] = static_cast<uint32_t>(cgltf_accessor_read_index(primitive.indices, v + i));
-                }
-            }
-
-            std::vector<Vertex> verts(primitive.attributes[0].data->count);
-            for (auto i = 0; i < primitive.attributes_count; i++)
-            {
-                const auto& attribute = primitive.attributes[i];
-
-                cgltf_size offset = 0;
-                switch (attribute.type)
-                {
-                case cgltf_attribute_type::cgltf_attribute_type_position:
-                    offset = offsetof(Vertex, pos);
-                    break;
-                case cgltf_attribute_type::cgltf_attribute_type_texcoord:
-                    offset = offsetof(Vertex, uv);
-                    break;
-                case cgltf_attribute_type::cgltf_attribute_type_normal:
-                    offset = offsetof(Vertex, normal);
-                    break;
-                default:
-                    spdlog::warn("[GltfLoader] Unhandled attribute {0}", attribute.name);
-                    continue;
-                }
-
-                auto numComponents = cgltf_num_components(attribute.data->type);
-                for (auto v = 0; v < attribute.data->count; v++)
-                {
-                    auto ptr = reinterpret_cast<uint8_t*>(&verts[v]) + offset;
-                    cgltf_accessor_read_float(attribute.data, v, reinterpret_cast<float*>(ptr), numComponents);
-                }
-            }
-
-            auto primitiveMesh = new Mesh;
-            primitiveMesh->name = p == 0 ? mesh.name : fmt::format("{}.{}", mesh.name, p);
-            primitiveMesh->vertexCount = static_cast<uint32_t>(verts.size());
-            primitiveMesh->indexCount = static_cast<uint32_t>(indices.size());
-            primitiveMesh->vertexBuffer = std::make_shared<Buffer>(sizeof(Vertex) * verts.size(), verts.data());
-            primitiveMesh->indexBuffer = std::make_shared<Buffer>(sizeof(uint32_t) * indices.size(), indices.data());
-
-            glCreateVertexArrays(1, &primitiveMesh->vao);
-
-            glVertexArrayVertexBuffer(primitiveMesh->vao, 0, primitiveMesh->vertexBuffer->buffer, 0, sizeof(Vertex));
-            glVertexArrayElementBuffer(primitiveMesh->vao, primitiveMesh->indexBuffer->buffer);
-
-            glEnableVertexArrayAttrib(primitiveMesh->vao, 0);
-            glEnableVertexArrayAttrib(primitiveMesh->vao, 1);
-            glEnableVertexArrayAttrib(primitiveMesh->vao, 2);
-
-            glVertexArrayAttribFormat(primitiveMesh->vao, 0, 3, GL_FLOAT, GL_FALSE, offsetof(Vertex, pos));
-            glVertexArrayAttribFormat(primitiveMesh->vao, 1, 2, GL_FLOAT, GL_FALSE, offsetof(Vertex, uv));
-            glVertexArrayAttribFormat(primitiveMesh->vao, 2, 3, GL_FLOAT, GL_FALSE, offsetof(Vertex, normal));
-
-            glVertexArrayAttribBinding(primitiveMesh->vao, 0, 0);
-            glVertexArrayAttribBinding(primitiveMesh->vao, 1, 0);
-            glVertexArrayAttribBinding(primitiveMesh->vao, 2, 0);
-
-            meshes.push_back(primitiveMesh);
-        }
-    }
-
-    std::copy(meshes.begin(), meshes.end(), std::back_inserter(outMeshes));
-
-    if (parentNode != nullptr)
-    {
-        auto createNode = [&meshes](opengl_starter::Node* parent, cgltf_node* n, auto& createNodeRef) -> void {
-            auto node = new opengl_starter::Node{ fmt::format("{}", n->name) };
-            node->pos = glm::make_vec3(n->translation);
-            node->scale = glm::make_vec3(n->scale);
-            node->rotq = glm::make_quat(n->rotation);
-
-            if (n->mesh != nullptr)
-            {
-                auto it = std::find_if(meshes.begin(), meshes.end(), [n](const Mesh* mesh) { return mesh->name == std::string(n->mesh->name); });
-                node->mesh = *it;
-            }
-
-            parent->children.push_back(node);
-
-            for (int m = 0; m < n->children_count; m++)
-            {
-                createNodeRef(node, n->children[m], createNodeRef);
-            }
-        };
-
-        auto sceneRoot = new opengl_starter::Node{ fmt::format("{}", data->scenes[0].name) };
-
-        for (int m = 0; m < data->scenes[0].nodes_count; m++)
-        {
-            const auto n = data->scenes[0].nodes[m];
-            createNode(sceneRoot, n, createNode);
+            spdlog::error("[cgltf] Failed to load file {}", filename);
+            return;
         }
 
-        parentNode->children.push_back(sceneRoot);
-    }
+        result = cgltf_load_buffers(&options, data, nullptr);
 
-    cgltf_free(data);
+        // Have another vector to avoid name collisions.
+        std::vector<opengl_starter::Mesh*> meshes;
+
+        for (int m = 0; m < data->meshes_count; m++)
+        {
+            const auto& mesh = data->meshes[m];
+            spdlog::info("{}, {}", mesh.name, mesh.primitives_count);
+            for (int p = 0; p < mesh.primitives_count; p++)
+            {
+                if (p > 0)
+                    spdlog::warn("[GltfLoader] {} has multiple primitives!", mesh.name);
+
+                const auto& primitive = mesh.primitives[p];
+
+                std::vector<uint32_t> indices(primitive.indices->count);
+                for (auto v = 0; v < primitive.indices->count; v += 3)
+                {
+                    for (auto i = 0; i < 3; i++)
+                    {
+                        indices[v + i] = static_cast<uint32_t>(cgltf_accessor_read_index(primitive.indices, v + i));
+                    }
+                }
+
+                std::vector<Vertex> verts(primitive.attributes[0].data->count);
+                for (auto i = 0; i < primitive.attributes_count; i++)
+                {
+                    const auto& attribute = primitive.attributes[i];
+
+                    cgltf_size offset = 0;
+                    switch (attribute.type)
+                    {
+                    case cgltf_attribute_type::cgltf_attribute_type_position:
+                        offset = offsetof(Vertex, pos);
+                        break;
+                    case cgltf_attribute_type::cgltf_attribute_type_texcoord:
+                        offset = offsetof(Vertex, uv);
+                        break;
+                    case cgltf_attribute_type::cgltf_attribute_type_normal:
+                        offset = offsetof(Vertex, normal);
+                        break;
+                    default:
+                        spdlog::warn("[GltfLoader] Unhandled attribute {0}", attribute.name);
+                        continue;
+                    }
+
+                    auto numComponents = cgltf_num_components(attribute.data->type);
+                    for (auto v = 0; v < attribute.data->count; v++)
+                    {
+                        auto ptr = reinterpret_cast<uint8_t*>(&verts[v]) + offset;
+                        cgltf_accessor_read_float(attribute.data, v, reinterpret_cast<float*>(ptr), numComponents);
+                    }
+                }
+
+                auto primitiveMesh = new Mesh;
+                primitiveMesh->name = p == 0 ? mesh.name : fmt::format("{}.{}", mesh.name, p);
+                primitiveMesh->vertexCount = static_cast<uint32_t>(verts.size());
+                primitiveMesh->indexCount = static_cast<uint32_t>(indices.size());
+                primitiveMesh->vertexBuffer = std::make_shared<Buffer>(sizeof(Vertex) * verts.size(), verts.data());
+                primitiveMesh->indexBuffer = std::make_shared<Buffer>(sizeof(uint32_t) * indices.size(), indices.data());
+
+                glCreateVertexArrays(1, &primitiveMesh->vao);
+
+                glVertexArrayVertexBuffer(primitiveMesh->vao, 0, primitiveMesh->vertexBuffer->buffer, 0, sizeof(Vertex));
+                glVertexArrayElementBuffer(primitiveMesh->vao, primitiveMesh->indexBuffer->buffer);
+
+                glEnableVertexArrayAttrib(primitiveMesh->vao, 0);
+                glEnableVertexArrayAttrib(primitiveMesh->vao, 1);
+                glEnableVertexArrayAttrib(primitiveMesh->vao, 2);
+
+                glVertexArrayAttribFormat(primitiveMesh->vao, 0, 3, GL_FLOAT, GL_FALSE, offsetof(Vertex, pos));
+                glVertexArrayAttribFormat(primitiveMesh->vao, 1, 2, GL_FLOAT, GL_FALSE, offsetof(Vertex, uv));
+                glVertexArrayAttribFormat(primitiveMesh->vao, 2, 3, GL_FLOAT, GL_FALSE, offsetof(Vertex, normal));
+
+                glVertexArrayAttribBinding(primitiveMesh->vao, 0, 0);
+                glVertexArrayAttribBinding(primitiveMesh->vao, 1, 0);
+                glVertexArrayAttribBinding(primitiveMesh->vao, 2, 0);
+
+                meshes.push_back(primitiveMesh);
+            }
+        }
+
+        std::copy(meshes.begin(), meshes.end(), std::back_inserter(outMeshes));
+
+        if (parentNode != nullptr)
+        {
+            auto createNode = [&meshes](opengl_starter::Node* parent, cgltf_node* n, auto& createNodeRef) -> void {
+                auto node = new opengl_starter::Node{ fmt::format("{}", n->name) };
+                node->pos = glm::make_vec3(n->translation);
+                node->scale = glm::make_vec3(n->scale);
+                node->rotq = glm::make_quat(n->rotation);
+
+                if (n->mesh != nullptr)
+                {
+                    auto it = std::find_if(meshes.begin(), meshes.end(), [n](const Mesh* mesh) { return mesh->name == std::string(n->mesh->name); });
+                    node->mesh = *it;
+                }
+
+                parent->children.push_back(node);
+
+                for (int m = 0; m < n->children_count; m++)
+                {
+                    createNodeRef(node, n->children[m], createNodeRef);
+                }
+            };
+
+            auto sceneRoot = new opengl_starter::Node{ fmt::format("{}", data->scenes[0].name) };
+
+            for (int m = 0; m < data->scenes[0].nodes_count; m++)
+            {
+                const auto n = data->scenes[0].nodes[m];
+                createNode(sceneRoot, n, createNode);
+            }
+
+            parentNode->children.push_back(sceneRoot);
+        }
+
+        cgltf_free(data);
+    }
 }
--- opengl_starter/TextRenderer.cpp
+++ opengl_starter/TextRenderer.cpp
@@ -5,193 +5,194 @@
 #include "Texture.h"
 #include "Utils.h"
 
-using namespace opengl_starter;
-
-const int MaxLetters = 1000;
-
-struct TextVertex
+namespace opengl_starter
 {
-    glm::vec3 pos;
-    glm::vec2 uv;
-    glm::vec4 color;
-};
+    const int MaxLetters = 1000;
 
-TextRenderer::TextRenderer(opengl_starter::Shader* textShader, opengl_starter::Texture* textTexture, opengl_starter::Font* font, int windowWidth, int windowHeight)
-    : _textShader(textShader), _textTexture(textTexture), _font(font),
-      _projMatrix(glm::ortho(0.0f, static_cast<float>(windowWidth), static_cast<float>(windowHeight), 0.0f, -1.0f, 1.0f))
-{
-    std::vector<uint32_t> indices;
-    indices.resize(MaxLetters * 6);
-    for (auto i = 0; i < MaxLetters; i++)
+    struct TextVertex
     {
-        indices[i * 6 + 1] = 3 + 4 * i;
-        indices[i * 6 + 0] = 2 + 4 * i;
-        indices[i * 6 + 2] = 0 + 4 * i;
+        glm::vec3 pos;
+        glm::vec2 uv;
+        glm::vec4 color;
+    };
 
-        indices[i * 6 + 4] = 2 + 4 * i;
-        indices[i * 6 + 3] = 1 + 4 * i;
-        indices[i * 6 + 5] = 0 + 4 * i;
+    TextRenderer::TextRenderer(opengl_starter::Shader* textShader, opengl_starter::Texture* textTexture, opengl_starter::Font* font, int windowWidth, int windowHeight)
+        : _textShader(textShader), _textTexture(textTexture), _font(font),
+          _projMatrix(glm::ortho(0.0f, static_cast<float>(windowWidth), static_cast<float>(windowHeight), 0.0f, -1.0f, 1.0f))
+    {
+        std::vector<uint32_t> indices;
+        indices.resize(MaxLetters * 6);
+        for (auto i = 0; i < MaxLetters; i++)
+        {
+            indices[i * 6 + 1] = 3 + 4 * i;
+            indices[i * 6 + 0] = 2 + 4 * i;
+            indices[i * 6 + 2] = 0 + 4 * i;
+
+            indices[i * 6 + 4] = 2 + 4 * i;
+            indices[i * 6 + 3] = 1 + 4 * i;
+            indices[i * 6 + 5] = 0 + 4 * i;
+        }
+
+        _vertexBuffer = std::make_shared<Buffer>(MaxLetters * 4 * sizeof(TextVertex));
+        _indexBuffer = std::make_shared<Buffer>(MaxLetters * 6 * sizeof(uint32_t), indices.data());
+
+        glCreateVertexArrays(1, &_vao);
+
+        glVertexArrayVertexBuffer(_vao, 0, _vertexBuffer->buffer, 0, sizeof(TextVertex));
+        glVertexArrayElementBuffer(_vao, _indexBuffer->buffer);
+
+        glEnableVertexArrayAttrib(_vao, 0);
+        glEnableVertexArrayAttrib(_vao, 1);
+        glEnableVertexArrayAttrib(_vao, 2);
+
+        glVertexArrayAttribFormat(_vao, 0, 3, GL_FLOAT, GL_FALSE, offsetof(TextVertex, pos));
+        glVertexArrayAttribFormat(_vao, 1, 2, GL_FLOAT, GL_FALSE, offsetof(TextVertex, uv));
+        glVertexArrayAttribFormat(_vao, 2, 4, GL_FLOAT, GL_FALSE, offsetof(TextVertex, color));
+
+        glVertexArrayAttribBinding(_vao, 0, 0);
+        glVertexArrayAttribBinding(_vao, 1, 0);
+        glVertexArrayAttribBinding(_vao, 2, 0);
+
+        _mappedBuffer = glMapNamedBufferRange(_vertexBuffer->buffer, 0, static_cast<GLintptr>(MaxLetters) * 4 * sizeof(TextVertex),
+            GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
     }
 
-    _vertexBuffer = std::make_shared<Buffer>(MaxLetters * 4 * sizeof(TextVertex));
-    _indexBuffer = std::make_shared<Buffer>(MaxLetters * 6 * sizeof(uint32_t), indices.data());
-
-    glCreateVertexArrays(1, &_vao);
-
-    glVertexArrayVertexBuffer(_vao, 0, _vertexBuffer->buffer, 0, sizeof(TextVertex));
-    glVertexArrayElementBuffer(_vao, _indexBuffer->buffer);
-
-    glEnableVertexArrayAttrib(_vao, 0);
-    glEnableVertexArrayAttrib(_vao, 1);
-    glEnableVertexArrayAttrib(_vao, 2);
-
-    glVertexArrayAttribFormat(_vao, 0, 3, GL_FLOAT, GL_FALSE, offsetof(TextVertex, pos));
-    glVertexArrayAttribFormat(_vao, 1, 2, GL_FLOAT, GL_FALSE, offsetof(TextVertex, uv));
-    glVertexArrayAttribFormat(_vao, 2, 4, GL_FLOAT, GL_FALSE, offsetof(TextVertex, color));
-
-    glVertexArrayAttribBinding(_vao, 0, 0);
-    glVertexArrayAttribBinding(_vao, 1, 0);
-    glVertexArrayAttribBinding(_vao, 2, 0);
-
-    _mappedBuffer = glMapNamedBufferRange(_vertexBuffer->buffer, 0, static_cast<GLintptr>(MaxLetters) * 4 * sizeof(TextVertex),
-        GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
-}
-
-TextRenderer::~TextRenderer()
-{
-    glUnmapNamedBuffer(_vertexBuffer->buffer);
-    glDeleteVertexArrays(1, &_vao);
-}
-
-void TextRenderer::Reset()
-{
-    _characters = 0;
-}
-
-void TextRenderer::ResizeWindow(int windowWidth, int windowHeight)
-{
-    _projMatrix = glm::ortho(0.0f, static_cast<float>(windowWidth), static_cast<float>(windowHeight), 0.0f, -1.0f, 1.0f);
-}
-
-void TextRenderer::RenderString(const std::string& text, const glm::mat4& model, float maxWidth, bool centered, float progress)
-{
-    if (progress == 0.0f)
-        return;
-
-    TextVertex* buffer = reinterpret_cast<TextVertex*>(_mappedBuffer);
-
-    glm::vec2 cursor = { 0.0f, 0.0f };
-
-    // cursor.y -= _font->lineHeight;
-
-    const float uvscale = 1.0f / _font->scaleW;
-
-    const int characterStart = _characters;
-
-    int currentChar = 0;
-    while (currentChar < text.length())
+    TextRenderer::~TextRenderer()
     {
-        auto [nextChars, lineWidth] = GetLineInfo(text, currentChar, maxWidth);
-        cursor.x = centered ? (-lineWidth / 2.0f) : 0.0f;
+        glUnmapNamedBuffer(_vertexBuffer->buffer);
+        glDeleteVertexArrays(1, &_vao);
+    }
 
-        for (auto i = currentChar; i < nextChars && (i + characterStart) < MaxLetters; i++)
+    void TextRenderer::Reset()
+    {
+        _characters = 0;
+    }
+
+    void TextRenderer::ResizeWindow(int windowWidth, int windowHeight)
+    {
+        _projMatrix = glm::ortho(0.0f, static_cast<float>(windowWidth), static_cast<float>(windowHeight), 0.0f, -1.0f, 1.0f);
+    }
+
+    void TextRenderer::RenderString(const std::string& text, const glm::mat4& model, float maxWidth, bool centered, float progress)
+    {
+        if (progress == 0.0f)
+            return;
+
+        TextVertex* buffer = reinterpret_cast<TextVertex*>(_mappedBuffer);
+
+        glm::vec2 cursor = { 0.0f, 0.0f };
+
+        // cursor.y -= _font->lineHeight;
+
+        const float uvscale = 1.0f / _font->scaleW;
+
+        const int characterStart = _characters;
+
+        int currentChar = 0;
+        while (currentChar < text.length())
         {
-            const auto& chr = _font->chars[static_cast<unsigned char>(text[i])];
+            auto [nextChars, lineWidth] = GetLineInfo(text, currentChar, maxWidth);
+            cursor.x = centered ? (-lineWidth / 2.0f) : 0.0f;
 
-            float curX = cursor.x + chr.xOffset;
-            float curY = cursor.y + chr.yOffset;
-            float dstX = curX + chr.width;
-            float dstY = curY + chr.height;
-
-            auto now = static_cast<float>(i) / static_cast<float>(text.length());
-            const auto color = glm::vec4{ 1.0f, 1.0f, 1.0f, 1.0f - glm::smoothstep(progress, progress + 0.1f, now) };
-
-            buffer[(_characters * 4) + 0].pos = { curX, dstY, 0.0f };
-            buffer[(_characters * 4) + 0].uv = { uvscale * chr.x, (uvscale * (chr.y + chr.height)) };
-            buffer[(_characters * 4) + 0].color = color;
-
-            buffer[(_characters * 4) + 1].pos = { dstX, dstY, 0.0f };
-            buffer[(_characters * 4) + 1].uv = { uvscale * (chr.x + chr.width), (uvscale * (chr.y + chr.height)) };
-            buffer[(_characters * 4) + 1].color = color;
-
-            buffer[(_characters * 4) + 2].pos = { dstX, curY, 0.0f };
-            buffer[(_characters * 4) + 2].uv = { uvscale * (chr.x + chr.width), (uvscale * chr.y) };
-            buffer[(_characters * 4) + 2].color = color;
-
-            buffer[(_characters * 4) + 3].pos = { curX, curY, 0.0f };
-            buffer[(_characters * 4) + 3].uv = { uvscale * chr.x, uvscale * chr.y };
-            buffer[(_characters * 4) + 3].color = color;
-
-            _characters++;
-
-            if (text.length() > 1 && i + 1 < text.length())
+            for (auto i = currentChar; i < nextChars && (i + characterStart) < MaxLetters; i++)
             {
-                const auto& kerning = _font->kernings.find(std::make_pair(text[i], text[i + 1]));
-                if (kerning != _font->kernings.end())
-                    cursor.x += kerning->second;
+                const auto& chr = _font->chars[static_cast<unsigned char>(text[i])];
+
+                float curX = cursor.x + chr.xOffset;
+                float curY = cursor.y + chr.yOffset;
+                float dstX = curX + chr.width;
+                float dstY = curY + chr.height;
+
+                auto now = static_cast<float>(i) / static_cast<float>(text.length());
+                const auto color = glm::vec4{ 1.0f, 1.0f, 1.0f, 1.0f - glm::smoothstep(progress, progress + 0.1f, now) };
+
+                buffer[(_characters * 4) + 0].pos = { curX, dstY, 0.0f };
+                buffer[(_characters * 4) + 0].uv = { uvscale * chr.x, (uvscale * (chr.y + chr.height)) };
+                buffer[(_characters * 4) + 0].color = color;
+
+                buffer[(_characters * 4) + 1].pos = { dstX, dstY, 0.0f };
+                buffer[(_characters * 4) + 1].uv = { uvscale * (chr.x + chr.width), (uvscale * (chr.y + chr.height)) };
+                buffer[(_characters * 4) + 1].color = color;
+
+                buffer[(_characters * 4) + 2].pos = { dstX, curY, 0.0f };
+                buffer[(_characters * 4) + 2].uv = { uvscale * (chr.x + chr.width), (uvscale * chr.y) };
+                buffer[(_characters * 4) + 2].color = color;
+
+                buffer[(_characters * 4) + 3].pos = { curX, curY, 0.0f };
+                buffer[(_characters * 4) + 3].uv = { uvscale * chr.x, uvscale * chr.y };
+                buffer[(_characters * 4) + 3].color = color;
+
+                _characters++;
+
+                if (text.length() > 1 && i + 1 < text.length())
+                {
+                    const auto& kerning = _font->kernings.find(std::make_pair(text[i], text[i + 1]));
+                    if (kerning != _font->kernings.end())
+                        cursor.x += kerning->second;
+                }
+
+                cursor.x += chr.xAdvance;
             }
 
-            cursor.x += chr.xAdvance;
+            cursor.y += _font->lineHeight;
+            currentChar = nextChars;
         }
 
-        cursor.y += _font->lineHeight;
-        currentChar = nextChars;
+        Render(model, characterStart, currentChar);
     }
 
-    Render(model, characterStart, currentChar);
-}
-
-void TextRenderer::Render(const glm::mat4& model, int characterStart, int characters)
-{
-    glBindProgramPipeline(_textShader->pipeline);
-    glProgramUniformMatrix4fv(_textShader->vertProg, glGetUniformLocation(_textShader->vertProg, "mvp"), 1, GL_FALSE, glm::value_ptr(_projMatrix * model));
-    glProgramUniform1i(_textShader->fragProg, glGetUniformLocation(_textShader->fragProg, "texSampler"), 0);
-    glBindTextureUnit(0, _textTexture->textureName);
-
-    glDisable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    glBindVertexArray(_vao);
-    glDrawElements(GL_TRIANGLES, characters * 6, GL_UNSIGNED_INT, reinterpret_cast<void*>(characterStart * 6 * sizeof(GLuint)));
-    glBindVertexArray(0);
-
-    glDisable(GL_BLEND);
-    glEnable(GL_DEPTH_TEST);
-}
-
-float TextRenderer::GetStringWidth(const std::string& text) const
-{
-    float result = 0;
-    for (const auto& c : text)
-        result += _font->chars[c].xAdvance;
-    return result;
-}
-
-std::pair<int, float> TextRenderer::GetLineInfo(const std::string& text, int currentLetter, float maxWidth) const
-{
-    if (maxWidth < 0.0f)
+    void TextRenderer::Render(const glm::mat4& model, int characterStart, int characters)
     {
-        return std::make_pair(static_cast<int>(text.length()), GetStringWidth(text));
+        glBindProgramPipeline(_textShader->pipeline);
+        glProgramUniformMatrix4fv(_textShader->vertProg, glGetUniformLocation(_textShader->vertProg, "mvp"), 1, GL_FALSE, glm::value_ptr(_projMatrix * model));
+        glProgramUniform1i(_textShader->fragProg, glGetUniformLocation(_textShader->fragProg, "texSampler"), 0);
+        glBindTextureUnit(0, _textTexture->textureName);
+
+        glDisable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        glBindVertexArray(_vao);
+        glDrawElements(GL_TRIANGLES, characters * 6, GL_UNSIGNED_INT, reinterpret_cast<void*>(characterStart * 6 * sizeof(GLuint)));
+        glBindVertexArray(0);
+
+        glDisable(GL_BLEND);
+        glEnable(GL_DEPTH_TEST);
     }
 
-    float currentWidth = 0.0f;
-    int prevLineBreak = currentLetter;
-    float widthAtLineBreak = 0.0f;
-    for (auto i = currentLetter; i < text.length(); i++)
+    float TextRenderer::GetStringWidth(const std::string& text) const
     {
-        if (text[i] == ' ' || text[i] == ',' || text[i] == '.' || text[i] == '!' || text[i] == '?')
+        float result = 0;
+        for (const auto& c : text)
+            result += _font->chars[c].xAdvance;
+        return result;
+    }
+
+    std::pair<int, float> TextRenderer::GetLineInfo(const std::string& text, int currentLetter, float maxWidth) const
+    {
+        if (maxWidth < 0.0f)
         {
-            prevLineBreak = i;
-            widthAtLineBreak = currentWidth;
+            return std::make_pair(static_cast<int>(text.length()), GetStringWidth(text));
         }
 
-        const auto& chr = _font->chars[static_cast<int>(text[i])];
-        currentWidth += chr.xAdvance;
-        if (currentWidth >= maxWidth)
+        float currentWidth = 0.0f;
+        int prevLineBreak = currentLetter;
+        float widthAtLineBreak = 0.0f;
+        for (auto i = currentLetter; i < text.length(); i++)
         {
-            return std::make_pair(prevLineBreak + 1, widthAtLineBreak);
+            if (text[i] == ' ' || text[i] == ',' || text[i] == '.' || text[i] == '!' || text[i] == '?')
+            {
+                prevLineBreak = i;
+                widthAtLineBreak = currentWidth;
+            }
+
+            const auto& chr = _font->chars[static_cast<int>(text[i])];
+            currentWidth += chr.xAdvance;
+            if (currentWidth >= maxWidth)
+            {
+                return std::make_pair(prevLineBreak + 1, widthAtLineBreak);
+            }
         }
+        return std::make_pair(static_cast<int>(text.length()), currentWidth);
     }
-    return std::make_pair(static_cast<int>(text.length()), currentWidth);
 }
